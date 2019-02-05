f24ffb0fb11107dedbe698184f3b5137a25f54bc
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-02-05 14:59:45 +0530

    0000 | Add Media directory info to organisation entry

--- create_organisation.sql
+++ create_organisation.sql
@@ -6,5 +6,5 @@ GRANT ALL ON ALL TABLES IN SCHEMA public TO ddm;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ddm;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO ddm;
 
-INSERT into organisation(name, db_user, uuid, parent_organisation_id)
-    SELECT 'Dam Desilting Mission', 'ddm', '750cf4e0-1909-47f0-8952-836ae4111c9b', id FROM organisation WHERE name = 'OpenCHS';
+INSERT into organisation(name, db_user, uuid, media_directory, parent_organisation_id)
+    SELECT 'Dam Desilting Mission', 'ddm', '750cf4e0-1909-47f0-8952-836ae4111c9b', 'dam_desilting', id FROM organisation WHERE name = 'OpenCHS';
