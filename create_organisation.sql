49a980c113362f01228bf0939bf1b62b682450ad
Vinay Venu <ijkvinay@gmail.com>
2019-02-26 12:16:47 +0530

    Move organisation to top level.

--- create_organisation.sql
+++ create_organisation.sql
@@ -7,4 +7,4 @@ GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ddm;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO ddm;
 
 INSERT into organisation(name, db_user, uuid, media_directory, parent_organisation_id)
-    SELECT 'Dam Desilting Mission', 'ddm', '750cf4e0-1909-47f0-8952-836ae4111c9b', 'dam_desilting', id FROM organisation WHERE name = 'OpenCHS';
+    values ('Dam Desilting Mission', 'ddm', '750cf4e0-1909-47f0-8952-836ae4111c9b', 'dam_desilting', null);
