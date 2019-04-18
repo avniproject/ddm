8aa339666ddc056ae286c5e1596a80ff4d61fc93
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-04-18 13:55:34 +0530

    0000 | Add migration/datafixes.sql file

---
+++ migration/datafixes.sql
@@ -0,0 +1,33 @@
+set role ddm;
+
+-- Void incorrect water body creation
+With audits as (update individual
+set is_voided = true where uuid in (SELECT ind.uuid
+                                    FROM individual ind
+                                          join audit a on ind.audit_id = a.id
+                                          join users creator on creator.id = a.created_by_id
+                                    where ind.is_voided = false
+                                      and creator.name <> 'dataimporter@ddm')
+returning audit_id)
+update audit set last_modified_date_time = current_timestamp where audit.id in (select audit_id from audits);
+
+-- Void enrolment if water body is_voided or the enrolment isn't imported by dataimporter@ddm
+with audits as (update program_enrolment
+set is_voided = true where uuid in (SELECT enl.uuid
+                                    FROM program_enrolment enl
+                                          join individual i on i.id = enl.individual_id
+                                          join audit a on enl.audit_id = a.id
+                                          join users creator on creator.id = a.created_by_id
+                                    where i.is_voided OR (enl.is_voided = false
+                                                  and creator.name <> 'dataimporter@ddm'))
+returning audit_id)
+update audit set last_modified_date_time = current_timestamp where audit.id in (select audit_id from audits);
+
+-- Void encounter if enrolment is_voided
+with audits as (update program_encounter
+set is_voided = true where id in (select enc.id
+                                    from program_encounter enc
+                                                join program_enrolment enl on enc.program_enrolment_id = enl.id
+                                    where enc.is_voided is false
+                                      and enl.is_voided is true) returning audit_id)
+update audit set last_modified_date_time = current_timestamp where audit.id in (select audit_id from audits);
