9d6726d35c58e05c81b954b4dddf991ca80b4365
Vinod Deolal <vinoddeolal@gmail.com>
2019-06-06 16:46:56 +0530

    0000 | add script to unexit incorrect program exits

--- migration/recurring_datafixes.sql
+++ migration/recurring_datafixes.sql
@@ -7,7 +7,7 @@ set is_voided = true where uuid in (SELECT ind.uuid
                                           join audit a on ind.audit_id = a.id
                                           join users creator on creator.id = a.created_by_id
                                     where ind.is_voided = false
-                                      and creator.name <> 'dataimporter@ddm')
+                                      and creator.username <> 'dataimporter@ddm')
 returning audit_id)
 update audit set last_modified_date_time = current_timestamp,
                  last_modified_by_id = (select id from users where username = 'dataimporter@ddm')
@@ -21,7 +21,7 @@ set is_voided = true where uuid in (SELECT enl.uuid
                                           join audit a on enl.audit_id = a.id
                                           join users creator on creator.id = a.created_by_id
                                     where i.is_voided OR (enl.is_voided = false
-                                                  and creator.name <> 'dataimporter@ddm'))
+                                                  and creator.username <> 'dataimporter@ddm'))
 returning audit_id)
 update audit set last_modified_date_time = current_timestamp,
                  last_modified_by_id = (select id from users where username = 'dataimporter@ddm')
@@ -36,4 +36,24 @@ set is_voided = true where id in (select enc.id
                                       and enl.is_voided is true) returning audit_id)
 update audit set last_modified_date_time = current_timestamp,
                  last_modified_by_id = (select id from users where username = 'dataimporter@ddm')
+where audit.id in (select audit_id from audits);
+
+--Unexit incorrect program exits
+with audits as (update program_enrolment
+  set program_exit_date_time = null ,
+    program_exit_observations = '{}',
+    exit_location = null
+  where uuid in (SELECT enl.uuid
+                 FROM program_enrolment enl
+                        join individual i on i.id = enl.individual_id
+                        join audit a on enl.audit_id = a.id
+                        join users creator on creator.id = a.created_by_id
+                        join users modifier on modifier.id = a.last_modified_by_id
+                 where enl.program_exit_date_time notnull
+                   and enl.is_voided = false
+                   and modifier.username <> 'dataimporter@ddm')
+  returning audit_id)
+update audit
+set last_modified_date_time = current_timestamp,
+    last_modified_by_id     = (select id from users where username = 'dataimporter@ddm')
 where audit.id in (select audit_id from audits);
